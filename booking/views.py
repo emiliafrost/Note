a7db586c0e7271392721369b9c0160351349568b
frostime <emilia9ash@gmail.com>
2018-12-21 11:39:14 +0800

    version: 3.0.1 @ 2018/12/21 with all files and ignore files settled set, cached are cleared so that .gitignore can work

--- booking/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-
-# Create your views here.
-def booking(request):
-    # return render(request,'login/index.html')
-    return HttpResponse('hello, this is booking')
